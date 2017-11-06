2e1316c0af768de2e4ca987f13676a7ce29bcaf5
gene-yoo <gene.y.yoo@gmail.com>
2017-11-05 21:03:20 -0500

    Done.

--- js/script.js
+++ js/script.js
@@ -1,36 +1,34 @@
 $(document).ready(function() {
-
-  lightActive();
-  redLight();
-  yellowLight();
-  greenLight();
-
-  
+	lightActive();
+	redLight();
+	yellowLight();
+	greenLight();
 });
 
-function lightActive(){
-  $('.light').on('click', function(){
-      $(this).toggleClass("active");
-      console.log("background purple")
-  });
+function lightActive() {
+	$(".light").on("click", function() {
+		$(this).toggleClass("active");
+		console.log("background purple");
+	});
 }
 
-function redLight(){
-  $(".redLight").on('click', function(){
-    $(this).toggleClass("red");
-  });
+function redLight() {
+	$(".redLight").on("click", function(ev) {
+		$(this).toggleClass("red");
+		ev.stopPropagation();
+	});
 }
 
-
-function yellowLight(){
-  $(".yellowLight").on('click', function(){
-    $(this).toggleClass("yellow");
-  });
+function yellowLight() {
+	$(".yellowLight").on("click", function(ev) {
+		$(this).toggleClass("yellow");
+		ev.stopPropagation();
+	});
 }
 
-function greenLight(){
-  $(".greenLight").on('click', function(){
-    $(this).toggleClass("green");
-  });
+function greenLight() {
+	$(".greenLight").on("click", function(ev) {
+		$(this).toggleClass("green");
+		ev.stopPropagation();
+	});
 }
-
